e969386df667392ab4666a93cbe7af795c6ea6de
Everson Silva <everson_gsilva@yahoo.com.br>
2021-10-27 14:04:13 -0300

    chore: new routes to data fake

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,10 @@ import ClienteController from "../modules/cliente/controllers/ClienteController"
 
 const routes = Router();
 
+routes.get("/fake", ClienteController.fakeData);
+
 routes.post("/cliente", ClienteController.store);
 routes.get("/clientes", ClienteController.index);
-routes.get("/cliente/findByName", ClienteController.findByName);
+routes.get("/clientes/cliente", ClienteController.findByName);
 
 export default routes;
