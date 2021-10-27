090089203639171befdf1c568c54de682940bf19
Everson Silva <everson_gsilva@yahoo.com.br>
2021-10-27 14:06:05 -0300

    chore: adjusts virtual data to Cliente

--- src/modules/Cliente/entities/Cliente.js
+++ src/modules/Cliente/entities/Cliente.js
@@ -2,21 +2,26 @@ import mongoose from "mongoose";
 
 const { Schema } = mongoose;
 
-const ClienteSchema = new Schema({
-  nome: {
-    type: String,
-    required: true,
+const ClienteSchema = new Schema(
+  {
+    nome: {
+      type: String,
+      required: true,
+    },
+    valor: {
+      type: Number,
+      required: true,
+    },
+    desde: {
+      type: Date,
+      required: true,
+      default: Date.now,
+    },
   },
-  valor: {
-    type: Number,
-    required: true,
-  },
-  desde: {
-    type: Date,
-    required: true,
-    default: Date.now,
-  },
-});
+  {
+    toJSON: { virtuals: true },
+  }
+);
 
 const Cliente = mongoose.model("Cliente", ClienteSchema);
 
